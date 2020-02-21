390e08fa5049b09671b6d5409a4abe9fd009cb37
AdinaGh <adina.lipsa@gmail.com>
2020-02-21 09:26:24 +0200

    add admin for recipes

---
+++ Web/StaticFiles/Scripts/adminrec.js
@@ -0,0 +1,16 @@
+﻿function gotoEdit(ahref) {
+    var id = $('#rec-id').val();
+    if (id == undefined) {
+        return false;
+    }
+    var url = $(ahref).attr('href');
+    if (url.endsWith('Edit')) {
+    }
+    else {
+   url =     $(ahref).attr('href').substr(0, $(ahref).attr('href').lastIndexOf('/'))
+    }
+    url = url + '/' + id;
+
+    $(ahref).attr('href', url);
+    return true;
+}
